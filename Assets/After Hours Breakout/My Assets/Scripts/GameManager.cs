f3bf3da200798e5f132ecc05331db920b1dbcb12
Dylan Fennelly <D00248176@student.dkit.ie>
2024-01-08 23:37:51 +0000

    Blocked out most of the basic map
    Blocked out most of the basic map, added some furniture, implemeneted
    the start of an event system for when door open so that the navmesh can
    be rebaked. Added animation for the most of the door and the power
    panel

---
+++ Assets/After Hours Breakout/My Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public NavMeshSurface surface;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {   
+            Destroy(gameObject);
+        }
+    }
+    public void OnDoorOpen()
+    {
+        BakeNavMesh();
+    
+    }
+
+    private void BakeNavMesh()
+    {
+        surface.BuildNavMesh();
+    }
+}
